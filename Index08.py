4f965ce356cfea8fd216588bc2c9b32154e18dba
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-11-29 01:18:22 +0500

    Index08

--- Index08.py
+++ Index08.py
@@ -6,5 +6,9 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    if s.find('*')!=-1:
+        return s.index('*')
+    else:
+        return False
+print(main('davrbn'))
         
